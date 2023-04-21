25af4b0b9e2c4c2e0b43cbe68c6cc9420b93fc2f
DAVID-OLU <ogunsanwodavid0@gmail.com>
2023-04-21 21:43:21 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# smtp-mail-sender
+
+# SMTP Mail Sender
+This is a mail sender python program, which takes in the receiver's email address as input and also the sender's email address, the subject of the message and the body of message itself.
+
+
+
+
+## Environment Variables
+
+To run this project, you will need to generate an app password, store the generated password in an environment variable key and then add it to the program source code.
+
+In my own case, its:
+
+`PASSWORD`
+
+
+
+
+## Tech Stack
+
+**Python**
+
+
+
+## Installation
+
+No installation needs to done as ssl and smtplib is already pre-installed in Python.
+
---
+++ emailSender.py
@@ -0,0 +1,32 @@
+# For the program to work, you need to : 
+# go over to your gmail account and setup 2 factor authentication.
+# generate app password. Then after that, store that generated password in an environment variable in your 
+# control panel(for security purpose. You dont want people to see your password.) and then use the variable here in your program code.
+
+
+from email.message import EmailMessage
+import ssl
+import smtplib
+
+
+email_sender = 'ogunsinqatar@gmail.com'
+email_password = input("")
+
+email_receiver = input("Enter Receiver Email: ")
+
+subject = input("Enter your Subject here: ")
+body = input("Enter your Message here: ")
+
+
+em = EmailMessage()
+em['From'] = email_sender
+em['To'] = email_receiver
+em['subject'] = subject
+em.set_content(body)
+
+context = ssl.create_default_context()
+
+with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+    smtp.login(email_sender, email_password)
+    smtp.sendmail(email_sender, email_receiver, em.as_string())
+
